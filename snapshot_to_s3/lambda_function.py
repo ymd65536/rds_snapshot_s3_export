6a8b809d3d64ddaf29ae82fc697acf07def33ad3
ymd65536 <ymd65536@gmail.com>
2022-09-05 17:51:35 +0900

    インデントサイズを変更

--- snapshot_to_s3/lambda_function.py
+++ snapshot_to_s3/lambda_function.py
@@ -8,28 +8,26 @@ rds = boto3.client('rds')
 s3 = boto3.resource('s3')
 client = boto3.client('kms')
 
-
 def export_snapshot(prefix, instanceid):
-    snapshots = rds.describe_db_snapshots(
-        DBInstanceIdentifier=instanceid, SnapshotType='manual')['DBSnapshots']
-    snapshots = sorted(
-        snapshots, key=lambda x: x['SnapshotCreateTime'], reverse=True)
-    snapshot = snapshots[0]
-    newsnapshotid = "-".join([prefix,
-                             datetime.now().strftime("%Y-%m-%d-%H-%M")])
+	snapshots = rds.describe_db_snapshots(
+	DBInstanceIdentifier=instanceid, SnapshotType='manual')['DBSnapshots']
+	snapshots = sorted(
+	snapshots, key=lambda x: x['SnapshotCreateTime'], reverse=True)
+	snapshot = snapshots[0]
+	newsnapshotid = "-".join([prefix,datetime.now().strftime("%Y-%m-%d-%H-%M")])
 
-    response = rds.start_export_task(
-        ExportTaskIdentifier=newsnapshotid,
-        SourceArn=snapshot['DBSnapshotArn'],
-        S3BucketName=[S3のバケット名],
-        IamRoleArn=[IAMロール]
-        KmsKeyId=[KMSkey],
-        S3Prefix=[S3に付けたいプレフィックス名],
-    )
-    return(response)
+	response = rds.start_export_task(
+	ExportTaskIdentifier=newsnapshotid,
+	SourceArn=snapshot['DBSnapshotArn'],
+	S3BucketName=[S3のバケット名],
+	IamRoleArn=[IAMロール]
+	KmsKeyId=[KMSkey],
+	S3Prefix=[S3に付けたいプレフィックス名],
+	)
+	return(response)
 
 
 def lambda_handler(event, context):
-    snapshot_prefix = [スナップショットのprefix名]
-    instance = [RDSのインスタンス名]
-    export_snapshot(snapshot_prefix, instance)
+	snapshot_prefix = [スナップショットのprefix名]
+	instance = [RDSのインスタンス名]
+	export_snapshot(snapshot_prefix, instance)
